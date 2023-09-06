2c7e610dbe25991cc163b2ebff55dfdd07f4bd70
PeachBlack-Alba <peachblackweb@gmail.com>
2023-09-06 19:15:52 +0200

    added receivers func

---
+++ deck.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// Create a new type of deck
+// which is a slice of strings
+
+type deck []string
+
+// This is a receiver function
+// print is the function name
+// deck is a reference to the type that we want
+// to attach this method to
+// any variable of type deck it gets access now to the
+// print method
+// the variable d is an instance of the variable deck
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
 	// card := "Ace of spades"
 	// var card string = "Ace of spades" => this is the same as the above,
@@ -13,15 +11,24 @@ func main() {
 	// card := newCard()
 
 	// Slice:
-	cards := []string{
+	/*cards := []string{
+		newCard(), newCard(),
+	}*/
+	// Since we created the deck in the deck.go file, we can just call it
+	// here instead of creating the whole slice
+	cards := deck{
 		newCard(), newCard(),
 	}
 	cards = append(cards, "Six of Spades")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-	fmt.Println(cards)
+	// We call the function from deck.go
+
+	cards.print()
+
+	//for i, card := range cards {
+	//	fmt.Println(i, card)
+	//}
+	//fmt.Println(cards)
 }
 
 func newCard() string {
